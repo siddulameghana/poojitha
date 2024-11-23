457255508cbf5f0710d833eabf48fb2b76647969
siddulameghana <siddulameghana827@gmail.com>
2024-11-23 09:54:52 +0530

    Update and rename README.md to charan.py

---
+++ charan.py
@@ -0,0 +1,13 @@
+# poojitha
+a = [23, 65, 19, 90]
+
+# Store element at index 1 (i.e., 65)
+temp = a[1]
+
+# Change element at index 1 to element at index 3 (i.e., 90)
+a[1] = a[3]
+
+# Change element at index 3 to temp (i.e., 65) 
+a[3] = temp
+
+print(a)
